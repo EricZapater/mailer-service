a548554b8276551e37f9d4f9b8af442b8dfab67e
EricZapater <ezapaterm@gmail.com>
2025-07-15 10:38:54 +0200

    Fix Action

--- internal/model/request.go
+++ internal/model/request.go
@@ -1,16 +1,17 @@
 package model
 
 type Attachment struct {
-	Filename string
-	Content  []byte
+	Filename    string `json:"filename" example:"document.pdf"`
+	Content     []byte `json:"content" swaggerignore:"true"`
+	ContentType string `json:"content_type" example:"application/pdf"`
 }
 
 type Request struct {
-	MailTo       string  `json:"mail_to"`
-	MailCc       *string `json:"mail_cc"`
-	Body         string  `json:"body"`
-	TemplateName string  `json:"template_name"`
-	Subject      string  `json:"subject"`
-	Footer       string  `json:"footer"`
-	Attachments  *[]Attachment
+	MailTo       string        `json:"mail_to" example:"user@example.com"`
+	MailCc       *string       `json:"mail_cc,omitempty" example:"cc@example.com"`
+	Subject      string        `json:"subject" example:"Nova factura"`
+	Body         string        `json:"body" example:"Contingut del email"`
+	TemplateName string        `json:"template_name" example:"invoice"`
+	Footer       string        `json:"footer" example:"Peu del email"`
+	Attachments  *[]Attachment `json:"attachments,omitempty"`
 }
